ffc056313568431de3ab0ecfd7e8ec26e82eb5b0
Elavarasan K <elavarasan@live.in>
2021-12-27 22:06:14 +0530

    Added MVC to the application

--- WebClient.BookStore/WebClient.BookStore/Startup.cs
+++ WebClient.BookStore/WebClient.BookStore/Startup.cs
@@ -16,6 +16,11 @@ namespace WebClient.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //it will add mvc in our application 
+            //services.AddMvc();
+
+            //it will add mvc in our application this is new features added after 3.1 version
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +31,82 @@ namespace WebClient.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+           
+
+           
+
+           /*app.Use(async (context, next) =>
+                {
+                    await context.Response.WriteAsync("Hello from my first middleware 1\n");
+                    await next();
+                    await context.Response.WriteAsync("Hello from my first middleware response\n");
+                }
+            );
+            app.Use(async (context, next) =>
+                {
+                    await context.Response.WriteAsync("Hello from my second middleware 2\n");
+                    await next();
+                    await context.Response.WriteAsync("Hello from my second middleware response\n");
+                }
+            );
+            app.Use(async (context, next) =>
+                {
+                    await context.Response.WriteAsync("Hello from my third middleware 3\n");
+                }
+            );*/
+
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            /*app.UseEndpoints(endPoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello My World!");
-                });
+                endPoints.Map("/",
+                    async context =>
+                    {
+                        await context.Response.WriteAsync("Hello World!");
+                    }
+                );
+            });
+            app.UseEndpoints(endPoints =>
+            {
+                endPoints.Map("/ela",
+                    async context =>
+                    {
+                        await context.Response.WriteAsync("Hello World Mr.Ela!");
+                    }
+                );
+            });
+
+            app.UseEndpoints(endPoints =>
+            {
+                endPoints.Map("/environment",
+                    async context =>
+                    {
+                        await context.Response.WriteAsync(env.EnvironmentName);
+                    }
+                );
+            });
+
+            app.UseEndpoints(endPoints =>
+            {
+                endPoints.Map("/check",
+                    async context =>
+                    {
+                        if(env.IsDevelopment())
+                            await context.Response.WriteAsync("Your environment is Development");
+                        else if(env.IsProduction())
+                            await context.Response.WriteAsync("Your environment is Production");
+                        else if(env.IsStaging())
+                            await context.Response.WriteAsync("Your environment is Staging");
+                        else
+                            await context.Response.WriteAsync("Your environment is "+env.EnvironmentName);
+
+                    }
+                );
+            });
+            */
+            app.UseEndpoints(endPoints =>
+            {
+                endPoints.MapDefaultControllerRoute();
             });
         }
     }
